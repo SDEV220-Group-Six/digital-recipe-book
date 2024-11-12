ff92397555bcb8b29069f820e6b35e6847ad71ac
Andrei Benea <andrei.benea89@gmail.com>
2024-11-12 08:37:35 -0500

    add main pages to recipes app

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home.html", views.home, name="home"),
+    path("", views.home, name="home"),
+    path("ingredients.html", views.ingredients, name="ingredients"),
+    path("recipes.html", views.recipes, name="recipes"),
+    path("shopping_list.html", views.shopping_list, name="shopping_list")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, "recipes/base.html")
+
+
+def ingredients(request):
+    return render(request, "recipes/ingredients.html")
+
+
+def recipes(request):
+    return render(request, "recipes/recipes.html")
+
+
+def shopping_list(request):
+    return render(request, "recipes/shopping_list.html")
