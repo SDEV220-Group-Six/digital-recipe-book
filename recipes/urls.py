b607219e63bf8909a3b2946028c67e34a19d3f10
Andrei Benea <andrei.benea89@gmail.com>
2024-11-18 15:18:53 -0500

    add rest endpoints(views) and routes(urls)

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home.html", views.home, name="home"),
-    path("ingredients.html", views.ingredients, name="ingredients"),
-    path("recipes.html", views.recipes, name="recipes"),
-    path("shopping_list.html", views.shopping_list, name="shopping_list"),
-    path("ingredients/list/", views.get_ingredients_list, name="ingredients_list"),
-    path("delete_ingredient/<int:ingredient_id>/", views.delete_ingredient, name="delete_ingredient"),
-    path("get_ingredient/<int:ingredient_id>/", views.get_ingredient, name="get_ingredient"),
+    path("", views.home, name="home"),
+    path("ingredients/", views.ingredients, name="ingredients"),
+    path("recipes/", views.recipes, name="recipes"),
+    path("shopping-list/", views.shopping_list, name="shopping-list"),
+]
+
+urlpatterns += [
+    path("api/ingredients/", views.IngredientListView.as_view(), name="api-ingredient-list"),
+    path("api/ingredients/<int:pk>/", views.IngredientDetailView.as_view(), name="api-ingredient-detail"),
 ]
