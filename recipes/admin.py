2bcdc7c4af643719fa7a81bb73fb5ced9fe48a48
Andrei Benea <andrei.benea89@gmail.com>
2024-11-25 10:21:32 -0500

    register Note to /admin, add STATIC_ROOT for prod

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import Ingredient, Note
 
 # Register your models here.
 
 admin.site.register(Ingredient)
+admin.site.register(Note)
